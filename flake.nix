b9464dd8de153a18012de00c01b82e0aefb7a501
nazozokc <nazozokc@gmail.com>
2026-03-04 16:28:07 +0900

    add flake.nix for nix install:20260304

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "CLI Pomodoro Timer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttr systems (system: f (nixpkgs.legacyPackages.${system}));
+    in
+    {
+      packages = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "pomodoro-cli";
+            version = "1.0.0";
+
+            src = ./.;
+
+            buildInputs = [
+              pkgs.nodejs_20
+              pkgs.pnpm
+            ];
+
+            buildPhase = ''
+              pnpm install --frozen-lockfile --ignore-scripts
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp src/index.js $out/bin/pomodoro
+              chmod +x $out/bin/pomodoro
+            '';
+
+            postFixup = ''
+              substituteInPlace $out/bin/pomodoro \
+                --replace '#!/usr/bin/env node' '#!${pkgs.nodejs_20}/bin/node'
+            '';
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs_20
+              pkgs.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
