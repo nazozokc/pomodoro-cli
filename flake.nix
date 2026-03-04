42bd26a6e8ad1cfa015fe475ea32dfdaa376c6d0
nazozokc <nazozokc@gmail.com>
2026-03-04 20:05:25 +0900

    github-workflows

--- flake.nix
+++ flake.nix
@@ -10,31 +10,22 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
     in
     {
       packages.${system}.default = pkgs.buildNpmPackage {
         pname = "pomodoro-cli";
         version = "1.2.0";
 
-        src = pkgs.lib.cleanSource ./.;
+        src = self;
 
         npmDepsHash = "sha256-Q38fJLAU7rYrA1EKSAx0L631rxx1Hnp4yPyAMLe2isQ=";
         nodejs = pkgs.nodejs_20;
-
         dontNpmBuild = true;
       };
 
       apps.${system}.default = {
         type = "app";
         program = "${self.packages.${system}.default}/bin/pomodoro";
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.nodejs_20
-          pkgs.npm
-        ];
       };
     };
 }
